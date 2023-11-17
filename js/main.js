4fb8cde37c9bb704b42d562408a78cc0a62502ac
Vikrant singh <82492082+vikrantsinghvr@users.noreply.github.com>
2023-11-17 19:32:54 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,197 @@
+(function ($) {
+    "use strict";
+    // // Dropdown on mouse hover
+    // $(document).ready(function () {
+    //     function toggleNavbarMethod() {
+    //         if ($(window).width() > 992) {
+    //             $('.navbar .dropdown').on('mouseover', function () {
+    //                 $('.dropdown-toggle', this).trigger('click');
+    //             }).on('mouseout', function () {
+    //                 $('.dropdown-toggle', this).trigger('click').blur();
+    //             });
+    //         } else {
+    //             $('.navbar .dropdown').off('mouseover').off('mouseout');
+    //         }
+    //     }
+    //     toggleNavbarMethod();
+    //     $(window).resize(toggleNavbarMethod);
+    // });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Main News carousel
+    $(".main-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        center: true,
+    });
+
+
+    // Tranding carousel
+    $(".tranding-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>'
+        ]
+    });
+
+
+    // Carousel item 1
+    $(".carousel-item-1").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ]
+    });
+
+    // Carousel item 2
+    $(".carousel-item-2").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+
+    // Carousel item 3
+    $(".carousel-item-3").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+
+    // Carousel item 4
+    $(".carousel-item-4").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+        }
+    });
+    
+})(jQuery);
+
+// $(function(){
+//     $('#searchbx').css("display",'none');
+//     $('#searchicon').click(function(){
+//         $('#searchbx').css("display",'block');
+        
+//     })
+// })
+// $(function(){
+//     $('#viewcount').
+// })
+var likes=0;
+    $('#viewcount').click(function(){
+        likes++;
+        $('#viewcountnum').text(likes);
+    })
+var likes1=0;
+    $('#viewcount1').click(function(){
+        likes1++;
+        $('#viewcountnum1').text(likes1);
+    })
+var likes2=0;
+    $('#viewcount2').click(function(){
+        likes2++;
+        $('#viewcountnum2').text(likes2);
+    })
+var likes3=0;
+    $('#viewcount3').click(function(){
+        likes3++;
+        $('#viewcountnum3').text(likes3);
+    })
+var likes4=0;
+    $('#viewcount4').click(function(){
+        likes4++;
+        $('#viewcountnum4').text(likes4);
+    })
